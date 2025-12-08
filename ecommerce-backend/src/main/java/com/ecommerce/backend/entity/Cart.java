d260e845c38959eb2e1ae53854e88c4038090fb5
abhi-codespace <one.abhi@outlook.com>
2025-12-08 21:12:39 +0530

    Creating Cart entity add values

---
+++ ecommerce-backend/src/main/java/com/ecommerce/backend/entity/Cart.java
@@ -0,0 +1,47 @@
+package com.ecommerce.backend.entity;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "carts")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cart {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(mappedBy = "cart")
+    private List<CartItem> cartItems;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate()
+    {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate()
+    {
+        updatedAt = LocalDateTime.now();
+    }
+}
