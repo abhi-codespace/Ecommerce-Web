b42144d5525b42fca3e4c190714f4e8f7e8b5001
abhi-codespace <one.abhi@outlook.com>
2025-12-08 13:35:02 +0530

    Created Product entity

---
+++ ecommerce-backend/src/main/java/com/ecommerce/backend/entity/Product.java
@@ -0,0 +1,75 @@
+package com.ecommerce.backend.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "products")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="product_name")
+    @NotBlank(message = "Product name is required")
+    private String productName;
+
+
+    @NotNull(message = "Price is required")
+    @Positive(message = "Price must be positive")
+    private Double price;
+
+    @Column(unique = true)
+    @NotBlank(message = "Brand is required")
+    private String brand;
+
+
+    @Column(length = 500)
+    private String description;
+
+    @Lob
+    @Column(name = "image_data")
+    private byte[] imageData;
+
+    @Column(name ="image_content_type")
+    private String imageContentType;
+
+    @Column(nullable = false)
+    private Boolean isActive = true;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate()
+    {
+       createdAt = LocalDateTime.now();
+       updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate()
+    {
+        updatedAt=LocalDateTime.now();
+    }
+
+}
