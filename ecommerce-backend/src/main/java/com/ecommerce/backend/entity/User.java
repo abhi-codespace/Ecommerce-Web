c785ff8c77e3d4ec789d6c7ff9c68d9642ad6735
abhi-codespace <one.abhi@outlook.com>
2025-12-08 21:22:38 +0530

    Mapping OneToMany for order ,cartItem entity

--- ecommerce-backend/src/main/java/com/ecommerce/backend/entity/User.java
+++ ecommerce-backend/src/main/java/com/ecommerce/backend/entity/User.java
@@ -1,10 +1,13 @@
 package com.ecommerce.backend.entity;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -39,4 +42,10 @@ public class User {
     @NotBlank(message = "Password is required")
     @Size(min=6, message = "Password must have at least 6 characters")
     private String password;   
+
+    @OneToMany(mappedBy = "user")
+    private List<Order> orders;
+
+    @OneToMany(mappedBy = "user")
+    private List<CartItem> cartItems;
 }
