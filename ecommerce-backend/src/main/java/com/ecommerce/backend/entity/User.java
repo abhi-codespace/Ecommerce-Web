c54dd9dae59b27229ff094a55f25013fbf41a25a
abhi-codespace <one.abhi@outlook.com>
2025-12-08 13:35:20 +0530

    Created User entity

---
+++ ecommerce-backend/src/main/java/com/ecommerce/backend/entity/User.java
@@ -0,0 +1,42 @@
+package com.ecommerce.backend.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 3,max = 20,message = "Username must be between 3 and 20 characters")
+    @Column(unique = true)
+    private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Please provide a valid email")
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min=6, message = "Password must have at least 6 characters")
+    private String password;   
+}
