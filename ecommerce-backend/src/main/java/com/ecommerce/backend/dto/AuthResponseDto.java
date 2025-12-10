9deda69c802fdbfbc2bda5db0ecf6022dc6f1222
abhi-codespace <one.abhi@outlook.com>
2025-12-10 23:22:29 +0530

    Creating AuthResposeDto

---
+++ ecommerce-backend/src/main/java/com/ecommerce/backend/dto/AuthResponseDto.java
@@ -0,0 +1,11 @@
+package com.ecommerce.backend.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class AuthResponseDto {
+    private String message;
+    private UserResponseDto user;
+}
