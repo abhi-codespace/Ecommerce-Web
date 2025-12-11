db7127b3907ddbb09cea3a9496ce682c300d510e
abhi-codespace <one.abhi@outlook.com>
2025-12-12 00:29:28 +0530

    Created CartController

---
+++ ecommerce-backend/src/main/java/com/ecommerce/backend/controller/CartController.java
@@ -0,0 +1,15 @@
+package com.ecommerce.backend.controller;
+
+public class CartController {
+    
+
+//     POST /api/cart/items – add product to cart (user from Authentication)
+
+// GET /api/cart – get current user cart
+
+// PUT /api/cart/items/{itemId} – update quantity
+
+// DELETE /api/cart/items/{itemId} – remove item
+
+// (Optional) DELETE /api/cart – clear cart
+}
