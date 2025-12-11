cf693f79ce8a67d6ad8cb5ec19523e9dc0607f7a
abhi-codespace <one.abhi@outlook.com>
2025-12-12 00:09:51 +0530

    Modified priceAtPurchase Double type into BigDecimal

--- ecommerce-backend/src/main/java/com/ecommerce/backend/dto/OrderItemSummaryDto.java
+++ ecommerce-backend/src/main/java/com/ecommerce/backend/dto/OrderItemSummaryDto.java
@@ -1,12 +1,16 @@
 package com.ecommerce.backend.dto;
 
+import java.math.BigDecimal;
+
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class OrderItemSummaryDto {
     private Long productId;
     private String productName;
     private Integer quantity;
-    private Double priceAtPurchase;
+    private BigDecimal priceAtPurchase;
 
 }
