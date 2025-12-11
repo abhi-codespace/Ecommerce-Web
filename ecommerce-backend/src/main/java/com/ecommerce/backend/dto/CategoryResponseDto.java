b8797c2af02fee34bf79e549fa74afb925fc9d78
abhi-codespace <one.abhi@outlook.com>
2025-12-12 00:05:12 +0530

    Created categoryResponseDto

---
+++ ecommerce-backend/src/main/java/com/ecommerce/backend/dto/CategoryResponseDto.java
@@ -0,0 +1,12 @@
+package com.ecommerce.backend.dto;
+
+import lombok.Data;
+
+@Data
+public class CategoryResponseDto {
+    
+    private Long id;
+    private String name;
+    private String description;
+    private Integer productCount;
+}
