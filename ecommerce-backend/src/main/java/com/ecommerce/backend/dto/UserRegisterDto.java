9bfc43541b1f63a9336af6b767869fbb4811ce25
abhi-codespace <one.abhi@outlook.com>
2025-12-12 00:12:25 +0530

    Created UserRegisterDto

---
+++ ecommerce-backend/src/main/java/com/ecommerce/backend/dto/UserRegisterDto.java
@@ -0,0 +1,22 @@
+package com.ecommerce.backend.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class UserRegisterDto {
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 20)
+    private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6)
+    private String password;
+}
